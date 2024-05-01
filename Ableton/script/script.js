26fc30abae980d8f24db6c33de4111d30aa45796
Quinn Bruckmann <quinnyb64@gmail.com>
2024-05-01 14:39:41 +1000

    Other project uploads

--- Ableton/script/script.js
+++ Ableton/script/script.js
@@ -1,2 +1,17 @@
 // all icons
-<script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>;
+{
+  /* <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>; */
+}
+
+// sets navigation extension to sticky when scrolling up
+var navbarExt = document.getElementById("extension-navigation");
+
+window.onscroll = function (e) {
+  if (this.oldScroll > this.scrollY) {
+    navbarExt.style.position = "sticky";
+    navbarExt.style.transform = "transform: translateY(120px, 50%)";
+  } else {
+    navbarExt.style.position = "relative";
+  }
+  this.oldScroll = this.scrollY;
+};
