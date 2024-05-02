045bbbab774d3cf98017d491df370e3a6019a97a
Quinn Bruckmann <quinnyb64@gmail.com>
2024-05-02 15:36:16 +1000

    dev work

--- Ableton/script/script.js
+++ Ableton/script/script.js
@@ -9,9 +9,23 @@ var navbarExt = document.getElementById("extension-navigation");
 window.onscroll = function (e) {
   if (this.oldScroll > this.scrollY) {
     navbarExt.style.position = "sticky";
-    navbarExt.style.transform = "transform: translateY(120px, 50%)";
   } else {
     navbarExt.style.position = "relative";
   }
   this.oldScroll = this.scrollY;
 };
+
+// set mega-menu functionality
+var moreBtn = document.getElementById("active");
+moreBtn.onclick = function () {
+  if (moreBtn.innerHTML == "More +") {
+    moreBtn.innerHTML = "More -";
+    document.getElementsByClassName("mega-menu-container")[0].style.display =
+      "block";
+  } else {
+    moreBtn.innerHTML = "More +";
+    document.getElementsByClassName("mega-menu-container")[0].style.display =
+      "none";
+  }
+};
+console.log(moreBtn.className);
